0cb2ca775a95d603774af286bd04365136301ac9
Risviantoe <risviantoe@gmail.com>
2021-11-12 10:32:04 +0700

    menambah seeder

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Admin::insert([
+            'nama' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('admin123'),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+    }
+}
